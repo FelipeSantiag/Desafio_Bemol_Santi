1d60568c83066ba22342c2ed9a682a60ecfceb61
FelipeSantiag <felipesantiago.job@gmail.com>
2022-08-22 07:01:03 -0400

    Implementação dos endpoints listagem e cadastro de clientes.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
+
+(async() => {
+const connectionString = 'mongodb://localhost:27017/bemol_santi';
+
+console.info('Conectando ao banco de dados MongoDB...');
+
+const options = {
+  useUnifiedTopology: true
+};
+
+const client = await mongodb.MongoClient.connect(connectionString, options);
+//console.log(client);
+
 const app = express();
 
 const port = 3000;
@@ -12,13 +27,13 @@ app.get('/', function (req, res) {
 
 /* //Modelo de objeto cliente
 let "cliente": {
-  //Dados cadastrais
+//Dados cadastrais
     //"id": 1,
     "nome": "Felipe Santiago",
     "dataNascimento": "05/09/1996",
     "genero":"masculino",
-    "Idcpf": "00000000-07",
-    //Dados de entrega
+    "Idcpf": "0000000007",
+//Dados de entrega
     "domicilio":"Casa",
     "cep":"69000000",
     "endereço": "Av. Alphaville",
@@ -26,20 +41,20 @@ let "cliente": {
     "cidade": "Manaus",
     "estado": "Amazonas",
     "referencia": "Igreja Adventista",
-    //Dados de contato
+//Dados de contato
     "Telcelular":'92982765182',
     "email": "felipesantiago.job@gmail.com ",
-    //Dados da conta
+//Dados da conta
     "senha": "@desafiobemol",
 };
 */
-
+/* //Clientes p/ Teste de API 
 const clientes = [
   { "id": 1,
     "nome": "Felipe Santiago",
     "dataNascimento": "05/09/1996",
     "genero":"masculino",
-    "Idcpf": "00000000-07",
+    "Idcpf": "0000000007",
     "domicilio":"Casa",
     "cep":"69000000",
     "endereço": "Av. Alphaville",
@@ -56,7 +71,7 @@ const clientes = [
     "nome": "Santiago",
     "dataNascimento": "09/05/1997",
     "genero":"masculino",
-    "Idcpf": "00000000-08",
+    "Idcpf": "0000000008",
     "domicilio":"Casa",
     "cep":"69000000",
     "endereço": "Av. Alphaville",
@@ -70,9 +85,15 @@ const clientes = [
   
   }
 ];
+*/
+const db = client.db('bemol_santi');
+const clientes = db.collection('clientes');
+const localEntrega = db.collection('localEntrega');
 
-const getClientesValidos = () =>  clientes.filter(Boolean);
-const getClienteByIdcpf = id => getClientesValidos().find(cliente => cliente.id === id);
+//console.log(await clientes.find({}).toArray());
+
+const getClientesValidos = () => clientes.find({}).toArray();
+const getClienteByIdcpf = id => clientes.findOne({ _id: ObjectId(id) });
 
 /*
 Lista de Endpoints da aplicação CRUD de clientes
@@ -86,31 +107,39 @@ CRUD: Criar, Ler (Individual e Tudo), Atualizar e Remover
 */
 
 //- [POST] /cadastro_cliente - Cria novo cadastro cliente
-app.post('/clientes', (req,res) => {
-  const cliente = req.body;
+app.post('/clientes', async (req,res) => {
+  const cadastro = req.body;
 
-  if(!clientes){
-    res.send('Cliente invalido');
+  if(!cadastro) { 
+      res.send('Cadastro invalido');
 
     return;
   }
-  cliente.id = clientes.length+1;
-  clientes.push(cliente);
-  res.send(cliente);
+
+  //cliente.id = clientes.length+1;
+  //clientes.push(cliente);
+    const dadosDaEntrega= await localEntrega.insertOne(cadastro.dadosEntrega);
+      
+    const dadosDoCliente = await clientes.insertOne(cadastro.dadosCliente);
+
+    dadosDoCliente.idLocalEntrega.update(dadosDaEntrega._id);    //PAREI AQUI
+     
+  console.log(cadastro);
+  //res.send(cliente);
   
 })
 
 //- [GET] /listagem_clientes - Retorna a lista de clientes
-app.get('/clientes', (req,res) => {
-  res.send(getClientesValidos());
+app.get('/clientes', async (req,res) => {
+  res.send(await getClientesValidos());
   //res.send(clientes);
 });
 
 //- [GET] /lista_cliente/{id} - Retorna apenas um unico cliente pelo ID
-app.get('/clientes/:id', (req,res) => {
-  const id = +req.params.id;
+app.get('/clientes/:id', async (req,res) => {
+  const id = req.params.id;
 
-  const cliente = getClienteByIdcpf(id);
+  const cliente = await getClienteByIdcpf(id);
 
   if(!cliente){
     res.send('Cliente não encontrado.');
@@ -160,4 +189,6 @@ app.delete('/clientes/:id', (req,res) => {
 
 app.listen(port, function(){
     console.info('App rodando em http://localhost:'+ port);
-});
+})
+
+})();
