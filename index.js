ca1390fb945006e9d39d6719d04041c8832290e2
FelipeSantiag <felipesantiago.job@gmail.com>
2022-08-22 00:49:44 -0400

    Criação da collection do POSTMAN.

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 });
 
-const clientes = [
-  {
-    //Dados cadastrais
+/* //Modelo de objeto cliente
+let "cliente": {
+  //Dados cadastrais
     //"id": 1,
     "nome": "Felipe Santiago",
     "dataNascimento": "05/09/1996",
@@ -31,11 +31,48 @@ const clientes = [
     "email": "felipesantiago.job@gmail.com ",
     //Dados da conta
     "senha": "@desafiobemol",
+};
+*/
+
+const clientes = [
+  { "id": 1,
+    "nome": "Felipe Santiago",
+    "dataNascimento": "05/09/1996",
+    "genero":"masculino",
+    "Idcpf": "00000000-07",
+    "domicilio":"Casa",
+    "cep":"69000000",
+    "endereço": "Av. Alphaville",
+    "bairro": "Cidade Nova",
+    "cidade": "Manaus",
+    "estado": "Amazonas",
+    "referencia": "Igreja Adventista",
+    "Telcelular":"92982765182",
+    "email": "felipesantiago.job@gmail.com ",
+    "senha": "@desafiobemol",
+
   },
+  { "id": 2,
+    "nome": "Santiago",
+    "dataNascimento": "09/05/1997",
+    "genero":"masculino",
+    "Idcpf": "00000000-08",
+    "domicilio":"Casa",
+    "cep":"69000000",
+    "endereço": "Av. Alphaville",
+    "bairro": "Cidade Nova",
+    "cidade": "Manaus",
+    "estado": "Amazonas",
+    "referencia": "Oficina batista",
+    "Telcelular":"92982765182",
+    "email": "santiago.dm@gmail.com ",
+    "senha": "@desafiobemol",
+  
+  }
 ];
 
-const getClientesValidos = () =>  clientes.filter(boolean);
-const getClienteByIdcpf = idcpf => getClientesValidos().find(cliente => clientes.Idcpf === idcpf);
+const getClientesValidos = () =>  clientes.filter(Boolean);
+const getClienteByIdcpf = id => getClientesValidos().find(cliente => cliente.id === id);
 
 /*
 Lista de Endpoints da aplicação CRUD de clientes
@@ -49,27 +86,29 @@ CRUD: Criar, Ler (Individual e Tudo), Atualizar e Remover
 */
 
 //- [POST] /cadastro_cliente - Cria novo cadastro cliente
-app.post('/cadastro-cliente', function(req,res){
+app.post('/clientes', (req,res) => {
   const cliente = req.body;
 
-  if(!cliente || !cliente.Idcpf){
+  if(!clientes){
     res.send('Cliente invalido');
 
     return;
   }
-  cliente.push(cliente);
+  cliente.id = clientes.length+1;
+  clientes.push(cliente);
   res.send(cliente);
   
 })
 
 //- [GET] /listagem_clientes - Retorna a lista de clientes
-app.get('/listagem-cliente', function(req,res){
+app.get('/clientes', (req,res) => {
   res.send(getClientesValidos());
+  //res.send(clientes);
 });
 
 //- [GET] /lista_cliente/{id} - Retorna apenas um unico cliente pelo ID
-app.get('/lista-cliente/:id', function(req,res){
-  const id = +req.params.idcpf;
+app.get('/clientes/:id', (req,res) => {
+  const id = +req.params.id;
 
   const cliente = getClienteByIdcpf(id);
 
@@ -81,24 +120,30 @@ app.get('/lista-cliente/:id', function(req,res){
 });
 
 //- [PUT] /atualiza_cliente/{id} - Atualiza uma cliente pelo ID
-app.put('/atualiza-cliente/:id', function(req,res){
-  const id = +req.params.Idcpf;
+app.put('/clientes/:id', (req,res) => {
+  const id = +req.params.id;
 
-  const cliente = getClienteByIdcpf(id);
+  //let index = getClienteByIdcpf(id).id;
 
   const novoCliente = req.body;
+
   if(!novoCliente){
     res.send('Cliente invalido');
     return;
   }
-  cliente=novoCliente;
+  const index = id-1;
+  delete getClienteByIdcpf(id);
+  novoCliente.id = id;
+  clientes[index]=novoCliente;
+  const cliente = getClienteByIdcpf(id);
+
   res.send(cliente);
 
 });
 
 //- [DELETE] /apaga_cliente/{id} - Remover um cliente pelo ID
-app.delete('/apaga-clientes/:id', function(req,res){
-  const id = +req.params.Idcpf;
+app.delete('/clientes/:id', (req,res) => {
+  const id = +req.params.id;
 
   const cliente = getClienteByIdcpf(id);
 
@@ -110,7 +155,7 @@ app.delete('/apaga-clientes/:id', function(req,res){
   const index = clientes.indexOf(cliente);
   delete clientes[index];
 
-  res.send('Mensagem removida com sucesso.');
+  res.send('Cliente removido com sucesso.');
 });
 
 app.listen(port, function(){
